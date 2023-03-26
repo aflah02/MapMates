e9774e8f94ace2c6cc63cb9b9137da1b20ba37ce
Aadit Kant Jha <aadit20001@iiitd.ac.in>
2023-03-26 23:58:12 +0530

    Basic User Interface

---
+++ frontend/MapMates/app/src/main/java/com/example/mapmates/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.example.mapmates
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton : Button = findViewById(R.id.log_in)
+        val signupRedirect : TextView = findViewById(R.id.signup_redirect)
+        val userNameView : TextView = findViewById(R.id.username_field)
+        val passwordView : TextView = findViewById(R.id.password_field)
+
+        signupRedirect.setOnClickListener{
+            val intent : Intent = Intent(applicationContext,SignupActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+
+        loginButton.setOnClickListener {
+            val userId : Int? = logInUser(userNameView.text.toString(),passwordView.text.toString())
+            if(userId == null){
+                Toast.makeText(applicationContext,"Invalid Credentials!",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //Successful Log-in
+                val intent : Intent = Intent(applicationContext,MainActivity::class.java)
+                intent.putExtra("UserId",userId)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            }
+        }
+    }
+    private fun logInUser(username : String, password: String): Int? {
+        //Make the API Call to log in here, fetch the id on successful login else null
+        return 1
+    }
+
+}
+
---
+++ frontend/MapMates/app/src/main/java/com/example/mapmates/SignupActivity.kt
@@ -0,0 +1,55 @@
+package com.example.mapmates
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.sign
+
+class SignupActivity : AppCompatActivity() {
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val loginRedirect: TextView = findViewById(R.id.login_redirect)
+        val signupButton : Button = findViewById(R.id.sign_up)
+        val userNameView : TextView = findViewById(R.id.username_field)
+        val passwordView : TextView = findViewById(R.id.password_field)
+        val emailView : TextView = findViewById(R.id.email_field)
+
+        loginRedirect.setOnClickListener{
+            val intent : Intent = Intent(applicationContext,LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+
+        signupButton.setOnClickListener {
+            val userId : Int? = signUpUser(userNameView.text.toString(),emailView.text.toString(),passwordView.text.toString())
+            if(userId == null){
+                Toast.makeText(applicationContext,"Cannot Signup!", Toast.LENGTH_SHORT).show()
+            }
+            else{
+
+                //Successful Sign-up
+                val intent : Intent = Intent(applicationContext,MainActivity::class.java)
+                intent.putExtra("UserId",userId)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            }
+        }
+    }
+
+    private fun signUpUser(username : String, email : String, password: String): Int? {
+        //Make the API Call to sign up here, fetch the id on successful login else null
+        return 1
+    }
+
+}
