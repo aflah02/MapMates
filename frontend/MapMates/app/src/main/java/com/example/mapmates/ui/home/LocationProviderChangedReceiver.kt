2919bc5587194b8449a4dab643d70fc8adce9efc
Ritwick01 <ritwick20461@iiitd.ac.in>
2023-03-27 15:52:11 +0530

    Current location is now available

---
+++ frontend/MapMates/app/src/main/java/com/example/mapmates/ui/home/LocationProviderChangedReceiver.kt
@@ -0,0 +1,33 @@
+package com.example.mapmates.ui.home
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import timber.log.Timber
+
+class LocationProviderChangedReceiver : BroadcastReceiver() {
+
+    internal var isGpsEnabled: Boolean = false
+    internal var isNetworkEnabled: Boolean = false
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent != null) {
+            intent.action?.let { act ->
+                if (act.matches("android.location.PROVIDERS_CHANGED".toRegex())) {
+                    val locationManager =
+                        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                    isNetworkEnabled =
+                        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+                    Timber.i("Location Providers changed, is GPS Enabled: " + isGpsEnabled)
+                    //Start your Activity if location was enabled:
+                    MyEventLocationSettingsChange.setChangeAndPost(isGpsEnabled) //notify
+                    //if (isGpsEnabled) {
+                    //Some action
+                    //}
+                }
+            }
+        }
+    }
+}
