ceba84f85b949bbb403e2226a3b037798d013d37
Ritwick01 <ritwick20461@iiitd.ac.in>
2023-04-20 02:16:37 +0530

    Need to add details about group settings and change location of FAB

---
+++ frontend/MapMates/app/src/main/java/com/example/mapmates/SettingsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mapmates
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        //receive intent
+        val intent = intent
+        //TODO: What all should be added to settings?
+
+    }
+}
